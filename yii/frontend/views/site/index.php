87754bcdd4ea78d1e8c9607816695eaa7fc81ea9
DimaForostenko <forostenkochampion@gmail.com>
2023-12-24 11:08:45 +0200

    add button

--- yii/frontend/views/site/index.php
+++ yii/frontend/views/site/index.php
@@ -2,29 +2,34 @@
 
 /** @var yii\web\View $this */
 
+
+use yii\helpers\Html as HelpersHtml;
+
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-    <div class="p-5 mb-4 bg-transparent rounded-3">
-        <div class="container-fluid py-5 text-center">
-            <h1 class="display-4">Congratulations!</h1>
-            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
-            <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-        </div>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-    </div>
- </div>
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Category</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="table-active">
+      <th scope="row">Active</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>
+        <span><?= HelpersHtml::a('View') ?></span>
+        <span><?= HelpersHtml::a('Update') ?></span>
+        <span><?= HelpersHtml::a('delete') ?></span>
+      </td>
+    </tr>
+  </tbody>
+</table>
+</div>
